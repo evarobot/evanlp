8680b1d07c96ab7202e4c3f2b2825b49c836285c
dev <local@dev>
2018-01-25 14:06:07 +0800

    添加配置文件

---
+++ vikinlu/config_sample.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+
+
+def init():
+    pass
+
+
+class DataConfig(object):
+    """"""
+    nlp_data_path = './'
+
+
+class ConfigLog(object):
+    log_level = 'INFO'
+    log_path = './'
+
+class ConfigRMQ(object):
+    host = '127.0.0.1'
+    port = '5672'
