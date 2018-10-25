8937f29fe8cf338fd1e2ebd3767a3de89db682e5
BITMAIN <bitmain@BITMAINdeMacBook-Pro.local>
2018-10-25 16:04:23 +0800

    update slot.py

--- vikinlu/slot.py
+++ vikinlu/slot.py
@@ -38,18 +38,16 @@ class SlotRecognizer(object):
         domain_id : str, Project id.
 
         """
-        ret = cms_gate.get_domain_slots(domain_id)
+        ret = cms_gate.get_slot_values_for_nlu(domain_id)
         if ret['code'] != 0:
             raise RuntimeError("Failed to invoke `get_domain_slots`.")
-        for slot in ret["slots"]:
+        slots = ret["data"]["slots"]
+        for name, values in slots.items():
             d_values = {}
-            ret = cms_gate.get_slot_values_for_nlu(slot['id'])
-            if ret['code'] != 0:
-                raise RuntimeError("Failed to invoke 'get_slot_values_for_nlu'")
-            for value in ret['data']['values']:
+            for value in values:
                 value["words"].append(value["name"])
                 d_values[value["name"]] = value["words"]
-            self._slots[slot["name"]] = d_values
+            self._slots[name] = d_values
 
     @classmethod
     def get_slot_recognizer(self, domain_id):
