8c56c357093c70111611e17ca307d3d2473398ed
wondereamer <wells7.wong@gmail.com>
2019-08-06 14:47:26 +0800

    update keywords ner

--- evanlp/ner/keyword.py
+++ evanlp/ner/keyword.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flashtext import KeywordProcessor
+#from flashtext import KeywordProcessor
+import re
 
 
 class KeyWordEntity(object):
@@ -26,33 +27,40 @@ class KeyWordEntity(object):
 
     @classmethod
     def _recognize_with_rules(self, text, cluewords):
-        flag_negative = False
-        pos_processor = KeywordProcessor()
-        neg_processor = KeywordProcessor()
-
-        for word in cluewords:
-            if word.startswith('-'):
-                neg_processor.add_keyword(word[1:])
-                flag_negative = True
-            else:
-                pos_processor.add_keyword(word)
-        if isinstance(text, list):
-            return list(set(text).intersection(set(cluewords)))
-
-        neg = []
-        pos = pos_processor.extract_keywords(text)
-
-        if flag_negative:
-            neg = neg_processor.extract_keywords(text)
-
-        if not neg:
-            # 如果没有遇到黑名单中的词，返回识别到的关键字
-            return pos
-        return []
+        # flag_negative = False
+        # pos_processor = KeywordProcessor()
+        # neg_processor = KeywordProcessor()
+        #
+        # for word in cluewords:
+        #     if word.startswith('-'):
+        #         neg_processor.add_keyword(word[1:])
+        #         flag_negative = True
+        #     else:
+        #         print(word)
+        #         pos_processor.add_keyword(word)
+        # if isinstance(text, list):
+        #     return list(set(text).intersection(set(cluewords)))
+        #
+        # neg = []
+        # pos = pos_processor.extract_keywords(text)
+        #
+        # if flag_negative:
+        #     neg = neg_processor.extract_keywords(text)
+        #
+        # if not neg:
+        #     # 如果没有遇到黑名单中的词，返回识别到的关键字
+        #     return pos
+        # return []
+        if isinstance(cluewords, str):
+            pattern = cluewords
+        else:
+            pattern = '|'.join(cluewords)
+        r = re.compile(pattern, flags=re.I | re.X)
+        return r.findall(text)
 
 
 if __name__ == '__main__':
-    print(KeyWordEntity.recognize("美元和黄金的相关性", ["美元", "黄金"]))
-    print(KeyWordEntity.recognize("美元和黄金2003的相关性", ["美元", "黄金"]))
-
+    print(KeyWordEntity.recognize("美元和黄金和美元的相关性", ["美元", "黄金"]))
+    print(KeyWordEntity.recognize("美元和黄金的相关性", "美元"))
+    print(KeyWordEntity.recognize("美元和黄金的相关性", "特别"))
 
